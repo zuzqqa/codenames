7339cba79c849b1030a0cffdaa7b07c65a6715fe
zuzannanowak <zuzannanowak04@wp.pl>
2024-12-25 00:12:48 +0100

    buttons almost done

---
+++ codenames-react-app/src/components/Button/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Button.css';
+
+function Button({ children, onClick, type = 'button', variant = 'primary', disabled = false }) {
+  return (
+    <button
+      className={`btn btn-${variant}`}
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
--- codenames-react-app/src/views/Home/Home.js
+++ codenames-react-app/src/views/Home/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-import BackgroundContainer from "../../containers/Background/Background";
-
 import "../../styles/App.css";
+import "../../styles/Home.css";
+
+import BackgroundContainer from "../../containers/Background/Background";
 import MenuContainer from "../../containers/Menu/Menu";
+
 import TitleComponent from "../../components/Title/Title";
 import SubtitleComponent from "../../components/Subtitle/Subtitle";
 import CharactersComponent from "../../components/Characters/Characters";
+import Button from "../../components/Button/Button";
 
 function Home() {
   return (
@@ -14,7 +17,20 @@ function Home() {
       <TitleComponent>Codenames</TitleComponent>
       <CharactersComponent />
       <SubtitleComponent>Your mission begins now</SubtitleComponent>
-      <MenuContainer></MenuContainer>
+      <MenuContainer>
+        <div className="first-column">
+          <div className="row1">
+            <Button variant="primary">Login</Button>
+          </div>
+          <div className="row2">
+            <Button variant="danger">Register</Button>
+          </div>
+        </div>
+        <div className="gold-bar"></div> 
+          <div className="second-column">
+            <Button variant="success">Play as Guest</Button>
+          </div>
+      </MenuContainer>
     </BackgroundContainer>
   );
 }
