f40c75bbc950a4fe374842a1775b433e5ecbf75b
zuzannanowak <zuzannanowak04@wp.pl>
2025-02-04 20:12:16 +0100

    RoomLobby is finished

--- codenames-frontend/src/components/RoomLobby/RoomLobby.tsx
+++ codenames-frontend/src/components/RoomLobby/RoomLobby.tsx
@@ -9,7 +9,7 @@ import Button from "../Button/Button.tsx";
 import backButton from "../../assets/icons/arrow-back.png";
 import playerIcon from "../../assets/images/player-icon.png";
 import "./RoomLobby.css";
-import {convertDurationToMMSS} from "../../shared/utils.tsx";
+import { convertDurationToMMSS } from "../../shared/utils.tsx";
 
 interface RoomLobbyProps {
   soundFXVolume: number;
@@ -84,7 +84,7 @@ const RoomLobby: React.FC<RoomLobbyProps> = ({ soundFXVolume }) => {
             setRedTeamPlayers(updatedGameSession.connectedUsers[0] || []);
             setBlueTeamPlayers(updatedGameSession.connectedUsers[1] || []);
 
-            if(updatedGameSession.status === SessionStatus.IN_PROGRESS) {
+            if (updatedGameSession.status === SessionStatus.IN_PROGRESS) {
               navigate("/choose-leader");
             }
           }
@@ -103,7 +103,7 @@ const RoomLobby: React.FC<RoomLobbyProps> = ({ soundFXVolume }) => {
       }
     };
   }, [navigate]);
-  
+
   const addPlayerToRedTeam = async () => {
     // Fetch player ID, then add to red team via REST API
     const storedGameId = localStorage.getItem("gameId");
@@ -159,11 +159,11 @@ const RoomLobby: React.FC<RoomLobbyProps> = ({ soundFXVolume }) => {
   const removePlayer = async () => {
     const storedGameId = localStorage.getItem("gameId");
     if (!storedGameId) return;
-    
+
     const getIdResponse = await fetch("http://localhost:8080/api/users/getId", {
-        method: "GET",
-        credentials: "include",
-      });
+      method: "GET",
+      credentials: "include",
+    });
     const userId = await getIdResponse.text();
 
     try {
@@ -171,7 +171,7 @@ const RoomLobby: React.FC<RoomLobbyProps> = ({ soundFXVolume }) => {
         `http://localhost:8080/api/game-session/${storedGameId}/disconnect?userId=${userId}`,
         { method: "DELETE", credentials: "include" }
       );
-  
+
       if (!response.ok) {
         console.error("Failed to remove player");
       }
@@ -218,15 +218,14 @@ const RoomLobby: React.FC<RoomLobbyProps> = ({ soundFXVolume }) => {
         <div className="background" style={{ gridColumn: "2", gridRow: "2" }}>
           {gameSession && (
             <div className="content">
-                <div className="game-name">{gameSession.gameName}</div>
+              <div className="game-name">{gameSession.gameName}</div>
               <div className="div-slots">
-                  {t("slots")}{" "}
-                  {gameSession.connectedUsers
-                    ? blueTeamPlayers.length +
-                      redTeamPlayers.length
-                    : 0}
-                  /{gameSession.maxPlayers}
-                </div>
+                {t("slots")}{" "}
+                {gameSession.connectedUsers
+                  ? blueTeamPlayers.length + redTeamPlayers.length
+                  : 0}
+                /{gameSession.maxPlayers}
+              </div>
               <div>{t("hint-duration")}</div>
               <div>{gameSession.timeForAHint}</div>
               <div>{t("guess-duration")}</div>
@@ -241,6 +240,10 @@ const RoomLobby: React.FC<RoomLobbyProps> = ({ soundFXVolume }) => {
                   <span className="button-text">Start</span>
                 </Button>
                 <div className="players-container">
+                  <div className="cloud-container">
+                    <div className="cloud blue-cloud"/>
+                    <div className="cloud red-cloud"/>
+                  </div>
                   <div className="team">
                     <Button
                       className="join-button"
