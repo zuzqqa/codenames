f6df249bc3907df06f6073e22a818ffbf41a21cb
zuzqqa <zuzannanowak04@wp.pl>
2025-02-26 16:52:14 +0100

    Windows tagging script

--- scripts/create_tag.bat
+++ scripts/create_tag.bat
@@ -8,15 +8,21 @@ git fetch --tags
 for /f "tokens=*" %%a in ('git describe --tags --abbrev^=0 origin/main') do set current_version=%%a
 echo Current version is: %current_version%
 
-:: Increment version based on user input
+:: Remove the "v" prefix if it exists
+set current_version=%current_version:v=%
+
+:: Remove any suffix (e.g., "-alpha", "-beta") from the version
+for /f "tokens=1 delims=-" %%a in ("%current_version%") do set clean_version=%%a
+
+:: Prompt user for version increment type
 echo Select version increment type:
 echo 1) Patch
 echo 2) Minor
 echo 3) Major
 set /p version_type=Enter your choice [1-3]:
 
-:: Divide version into parts
-for /f "tokens=1,2,3 delims=." %%a in (%current_version%) do (
+:: Split the version into major, minor, and patch numbers
+for /f "tokens=1,2,3 delims=." %%a in ("%clean_version%") do (
     set major=%%a
     set minor=%%b
     set patch=%%c
@@ -43,5 +49,5 @@ set new_tag=%new_version%-alpha
 echo Selected version: %new_tag%
 
 :: Save the selected version to a file instead of creating a tag
-echo %new_tag% > selected_version.txt
+echo v%new_tag% > selected_version.txt
 echo Version saved to selected_version.txt. Please create a Pull Request.
