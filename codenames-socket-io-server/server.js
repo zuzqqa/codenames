2d25a610c96826deffd05db3b3187a1080cf0084
zuzqqa <zuzannanowak04@wp.pl>
2025-05-26 21:10:03 +0200

    socket server cors

--- codenames-socket-io-server/server.js
+++ codenames-socket-io-server/server.js
@@ -7,8 +7,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "https://codenames-frontend-35261654330.us-central1.run.app",
-      "https://codenames-backend-35261654330.us-central1.run.app",
+      "https://codenames-frontend-35261654330.europe-central2.run.app",
+      "https://codenames-backend-35261654330.europe-central2.run.app",
     ],
     methods: ["GET", "POST"],
     credentials: true,
